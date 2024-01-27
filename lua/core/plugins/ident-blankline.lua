887ef28bc2904b84d558f6e03503911e48dfbd12
Martinezcabj12 <martinezcabj12@gmail.com>
2024-01-26 21:13:15 -0300

    [ident-blankline] cambio en los colores

--- lua/core/plugins/ident-blankline.lua
+++ lua/core/plugins/ident-blankline.lua
@@ -9,6 +9,7 @@ local highlight = {
     "RainbowViolet",
     "RainbowCyan",
 }
+
 local hooks = require "ibl.hooks"
 -- create the highlight groups in the highlight setup hook, so they are reset
 -- every time the colorscheme changes
@@ -22,7 +23,4 @@ hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
     vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
 end)
 
-vim.g.rainbow_delimiters = { highlight = highlight }
-require("ibl").setup { scope = { highlight = highlight } }
-
-hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
+require("ibl").setup { indent = { highlight = highlight } }
